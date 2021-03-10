62a91a2dcfeefac3b1036fe80a82da6f1ba11cb3
Abhishek singh <abhisheksingh@monocept.com>
2021-03-10 10:42:36 +0530

    LE SWP Product implementation

---
+++ DolphinApiTest/src/main/java/com/MLI_DOLPHIN/stepDefination/LE_SWP_Service.java
@@ -0,0 +1,169 @@
+package com.MLI_DOLPHIN.stepDefination;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
+import org.hamcrest.Matchers;
+import com.MLI_DOLPHIN.baseclass.WebservicesMethod;
+import com.MLI_DOLPHIN.utilities.ReusableFunction;
+import cucumber.api.DataTable;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.restassured.response.Response;
+import junit.framework.Assert;
+
+public class LE_SWP_Service {
+	
+	private final static Logger logger = Logger.getLogger(LE_SWP_Service.class.getName());
+	private int getSecondrowData;
+	private String endPointUlr;
+	private String header;
+	private String requestFile;
+	private String expResponseStatusCode;
+	private String actResponseStatusCode;
+	private String expResponseMsgCode;
+	private String expResponseMessage;
+	private String expATPvalue;
+	private String requestBody;
+	private Response responseBody;
+	private String  correlationId;
+	private String  comittedPremium;
+	private String  productName;
+	
+		
+	@Given("^Set the pre request test data for SWP$")
+	public void set_the_pre_request_test_data_for_SWP(DataTable preSetofData) throws Throwable {
+     List<String> listPreSetData =preSetofData.asList(String.class);
+     List<List<String>> numOfRows =preSetofData.raw();
+     getSecondrowData=listPreSetData.size()/numOfRows.size();
+     for(int i =getSecondrowData;i<listPreSetData.size();i++){
+    	 endPointUlr=listPreSetData.get(i);
+    	 i++;
+    	header= listPreSetData.get(i);
+    	i++;
+    	requestFile=listPreSetData.get(i);
+    	i++;
+    	correlationId=listPreSetData.get(i);
+    	i++;
+    	comittedPremium=listPreSetData.get(i);
+    	i++;
+    	productName=listPreSetData.get(i);
+    	
+     }
+ }
+	public String  set_input_data_in_Request(){
+		try {
+			requestBody=ReusableFunction.readJsonFile(requestFile);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		requestBody=requestBody.replaceAll(Pattern.quote("{{"+"X-Correlation-ID"+"}}"),correlationId );
+		requestBody=requestBody.replaceAll(Pattern.quote("{{"+"committedPremium"+"}}"),comittedPremium );
+		requestBody=requestBody.replaceAll(Pattern.quote("{{"+"productName"+"}}"),productName );
+        logger.info("All data set into request successfully..");
+		return requestBody;
+	}
+
+	@Given("^To set the expected data$")
+	public void to_set_the_expected_data(DataTable expSetofData) throws Throwable {
+	     List<String> listPreSetData =expSetofData.asList(String.class);
+	     List<List<String>> numOfRows =expSetofData.raw();
+	     getSecondrowData=listPreSetData.size()/numOfRows.size();
+	     for(int i =getSecondrowData;i<listPreSetData.size();i++){
+	    	 expResponseStatusCode=listPreSetData.get(i);
+	    	 i++;
+	    	expResponseMsgCode= listPreSetData.get(i);
+	    	i++;
+	    	expResponseMessage=listPreSetData.get(i);
+	    	i++;
+	    	expATPvalue=listPreSetData.get(i);
+	     }
+			logger.info("Expected data set Successfully...");
+	 }
+
+	@When("^Send the POST request for SWP$")
+	public void send_the_POST_request_for_SWP() throws Throwable {
+		
+		responseBody = WebservicesMethod.POST_METHOD(endPointUlr,set_input_data_in_Request(),
+				ReusableFunction.requestHeaders(header));
+		logger.info("Response Body is ::" + responseBody.prettyPrint());
+
+	}
+
+	@Then("^Lets Validate the response code for SWP$")
+	public void lets_Validate_the_response_code_for_SWP() throws Throwable {
+		actResponseStatusCode = String.valueOf(responseBody.getStatusCode());
+		Assert.assertEquals(expResponseStatusCode, actResponseStatusCode);
+		logger.info("Varification of status code is successfully pass:");
+
+	}
+
+	@Then("^Lets Validate response Msg Code and message for SWP$")
+	public void lets_Validate_response_Msg_Code_and_message_for_SWP() throws Throwable {
+     if(expResponseMsgCode.equals("200")){
+    	 responseBody.then().root("msgInfo").body("msgCode", Matchers.equalTo(expResponseMsgCode))
+    	 .and()
+    	 .body("msg", Matchers.equalTo(expResponseMessage));
+    	 logger.info("Validation of response message code is "+ expResponseMsgCode + "and message is "+expResponseMessage);
+    	 
+     }else if(expResponseMsgCode.equals("500")){
+       	 responseBody.then().root("msgInfo").body("msgCode", Matchers.equalTo(expResponseMsgCode))
+    	 .and()
+    	 .body("msg", Matchers.equalTo(expResponseMessage));
+       	 logger.info("Validation of response message code is "+ expResponseMsgCode + "and message is "+expResponseMessage);
+
+    	 
+     }else if(expResponseMsgCode.equals("400")){
+       	 responseBody.then().root("msgInfo").body("msgCode", Matchers.equalTo(expResponseMsgCode))
+    	 .and()
+    	 .body("msg", Matchers.equalTo(expResponseMessage));
+       	 logger.info("Validation of response message code is "+ expResponseMsgCode + "and message is "+expResponseMessage);
+
+    	 
+     }else{
+       	 logger.info("SWP API not response any data..");
+       }
+}
+
+	@Then("^Lets validate calculated AFYP value$")
+	public void lets_validate_calculated_ATP_value() throws Throwable {
+     	 responseBody.then().root("payload.premiumAmount").body("AFYP", Matchers.equalTo(expATPvalue));
+         logger.info("Validation of AFYP is "+expATPvalue);
+	}
+	
+	@Given("^Set the endPoint url\"([^\"]*)\"$")
+	public void set_the_endPoint_url(String urlEndPoint) throws Throwable {
+    endPointUlr=urlEndPoint;
+    logger.info("End point set successfully...");
+	}
+
+	@Then("^Lets validate illustration is generated or not$")
+	public void lets_validate_illustration_is_generated_or_not() throws Throwable {
+    responseBody.then().root("payload").body("illustrationPdfBase64", Matchers.notNullValue());
+    logger.info("validation of illustration value is not..");
+	}
+	@Given("^Set the premium in request\"([^\"]*)\"$")
+	public void set_the_input_data(String inputData) throws Throwable {		
+    comittedPremium=inputData;
+    logger.info("Committed premium set successfully in request");
+	}
+	@Given("^Set the correlationID\"([^\"]*)\"$")
+	public void set_the_correlationID(String inputCorrelationId) throws Throwable {
+		correlationId=inputCorrelationId;
+		logger.info("Correlation id set successfully..");
+	}
+	@Given("^Set the header\"([^\"]*)\"$")
+	public void set_the_header(String inputHeader) throws Throwable {
+    header=inputHeader;
+    logger.info("user set header as null..");
+	}
+
+
+
+
+
+}
---
+++ DolphinApiTest/src/test/java/com/MLI_DOLPHIN/featurefile/LE_SWP_Service.feature
@@ -0,0 +1,135 @@
+@SWP_ServiceTest
+Feature: To test the functionality of SWP product for premium and illustration
+Background:
+Given Set the pre request test data for SWP
+|EndPointUrl          |Header                                              |RequestFile         |correlationId  |commitedPremium |productName                |
+|/developer/lepremium |x-api-key:DTUDHv9UVG8cVT3qmhiSv1UcnvCduzLf1CI6zCVY  |LE_SWP_Service.json |123433         |260000          |Max Life Smart Wealth Plan |
+
+@SWP_PositiveTest001
+Scenario: To calculate the premium when pass premium 125000 for SWP
+Given To set the expected data 
+|ExpResponseCode|ExpResponseMsgCode|ExpResponseMessage|ExpATP       |
+|200            |200               |Success           |520000.00    | 
+When Send the POST request for SWP
+Then Lets Validate the response code for SWP
+And Lets Validate response Msg Code and message for SWP
+And Lets validate calculated AFYP value 
+
+@SWP_PositiveTest002
+Scenario Outline: To generate illustration for SWP
+Given To set the expected data 
+|ExpResponseCode|ExpResponseMsgCode|ExpResponseMessage|ExpATP       |
+|200            |200               |Success           |             | 
+Given Set the endPoint url"<endpoint_URL>"
+When Send the POST request for SWP
+Then Lets Validate the response code for SWP
+And Lets Validate response Msg Code and message for SWP
+And Lets validate illustration is generated or not
+
+Examples:
+|endpoint_URL              |
+|/developer/leillustration | 
+
+@SWP_PositiveTest003
+Scenario Outline: : To generate the AFYP when user pass premium 160000
+Given To set the expected data 
+|ExpResponseCode|ExpResponseMsgCode|ExpResponseMessage|ExpAFYP       |
+|200            |200               |Success           | 320000.00    | 
+
+Given Set the premium in request"<committedPremium>"
+When Send the POST request for SWP
+Then Lets Validate the response code for SWP
+And Lets Validate response Msg Code and message for SWP
+And Lets validate calculated AFYP value
+
+Examples:
+|committedPremium |
+|160000           |
+
+@SWP_PositiveTest004
+Scenario Outline: : To generate the illustration when user pass premium 160000
+Given To set the expected data 
+|ExpResponseCode|ExpResponseMsgCode|ExpResponseMessage|ExpAFYP  |
+|200            |200               |Success           |         | 
+Given Set the premium in request"<committedPremium>"
+Given Set the endPoint url"<endpoint_URL>"
+When Send the POST request for SWP
+Then Lets Validate the response code for SWP
+And Lets Validate response Msg Code and message for SWP
+And Lets validate illustration is generated or not
+Examples:
+|committedPremium  |endpoint_URL              |
+|160000            |/developer/leillustration | 
+
+@SWP_Negative_01
+Scenario Outline: : To generate the AFYP when user pass premium -160000
+Given To set the expected data 
+|ExpResponseCode|ExpResponseMsgCode|ExpResponseMessage|ExpAFYP  |
+|200            |500               |Failure           |         | 
+
+Given Set the premium in request"<committedPremium>"
+When Send the POST request for SWP
+Then Lets Validate the response code for SWP
+And Lets Validate response Msg Code and message for SWP
+Examples:
+|committedPremium  |
+|-160000           |
+
+@SWP_Negative_02
+Scenario Outline: : To generate the illustration when user pass premium -160000
+Given To set the expected data 
+|ExpResponseCode|ExpResponseMsgCode|ExpResponseMessage|ExpAFYP  |
+|200            |500               |Failure           |         | 
+Given Set the premium in request"<committedPremium>"
+Given Set the endPoint url"<endpoint_URL>"
+When Send the POST request for SWP
+Then Lets Validate the response code for SWP
+And Lets Validate response Msg Code and message for SWP
+Examples:
+|committedPremium  |endpoint_URL               |
+|-160000            |/developer/leillustration | 
+
+@SWP_Negative_03
+Scenario Outline: : To test the functionality of SWP when user pass correlation id as null
+Given To set the expected data 
+|ExpResponseCode|ExpResponseMsgCode|ExpResponseMessage|ExpAFYP  |
+|200            |400               |Bad Request       |         | 
+Given Set the correlationID"<CorrelationID>"
+When Send the POST request for SWP
+Then Lets Validate the response code for SWP
+And Lets Validate response Msg Code and message for SWP
+Examples:
+|CorrelationID |
+|              |
+
+@SWP_Negative_04
+Scenario Outline: : To test the functionality of SWP when user pass header as null
+Given To set the expected data 
+|ExpResponseCode|ExpResponseMsgCode|ExpResponseMessage|ExpAFYP  |
+|403            |                  | Forbiden         |         | 
+Given Set the header"<header>"
+When Send the POST request for SWP
+Then Lets Validate the response code for SWP
+Examples:
+|header          |
+|x-api-key:      |
+
+@SWP_Negative_05
+Scenario Outline: : To generate the illustration when user pass invalid endPoint
+Given To set the expected data 
+|ExpResponseCode|ExpResponseMsgCode|ExpResponseMessage|ExpAFYP  |
+|403            |                  |                  |         | 
+
+Given Set the endPoint url"<endpoint_URL>"
+When Send the POST request for SWP
+Then Lets Validate the response code for SWP
+
+Examples:
+ |endpoint_URL                 |
+ |/developer/leillustrationsed | 
+
+
+
+
+ 
+ 
