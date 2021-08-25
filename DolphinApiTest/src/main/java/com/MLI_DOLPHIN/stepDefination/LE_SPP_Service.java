8d4f365a533e89c832b9b85779447a85774ebcf9
Abhishek singh <abhisheksingh@monocept.com>
2021-08-25 13:49:41 +0530

    Spp Plan added in script

---
+++ DolphinApiTest/src/main/java/com/MLI_DOLPHIN/stepDefination/LE_SPP_Service.java
@@ -0,0 +1,153 @@
+package com.MLI_DOLPHIN.stepDefination;
+
+import java.util.List;
+import java.util.regex.Pattern;
+import org.apache.log4j.Logger;
+import org.hamcrest.Matchers;
+import com.MLI_DOLPHIN.baseclass.WebservicesMethod;
+import com.MLI_DOLPHIN.utilities.ReusableFunction;
+import cucumber.api.DataTable;
+import cucumber.api.java.en.*;
+import io.restassured.response.Response;
+
+public class LE_SPP_Service {
+	private final static Logger logger = Logger.getLogger(LE_SPP_Service.class.getName());
+	private String endPointUrl;
+	private String header;
+	private String requestFile;
+	private String requestBody;
+	private String responseMSgCode;
+	private String responseMsg;
+	private String ageOfInsured;
+	private String purchasePrice;
+	private String productName;
+	private String channelName;
+	private List<String> listOfSetData;
+	private List<List<String>> numberOfRow;
+	private int getSecondRowdata;
+	private Response responseBody;
+
+	@Given("^Set the prerequiest set of data for LE SPP Service$")
+	public void set_the_prerequiest_set_of_data_for_LE_SPP_Service(DataTable inputPreRequestData) throws Throwable {
+		listOfSetData = inputPreRequestData.asList(String.class);
+		numberOfRow = inputPreRequestData.raw();
+		getSecondRowdata = listOfSetData.size() / numberOfRow.size();
+		for (int i = getSecondRowdata; i < listOfSetData.size(); i++) {
+			endPointUrl = listOfSetData.get(i);
+			i++;
+			header = listOfSetData.get(i);
+			i++;
+			requestFile = listOfSetData.get(i);
+			i++;
+			ageOfInsured = listOfSetData.get(i);
+			i++;
+			purchasePrice = listOfSetData.get(i);
+			i++;
+			productName = listOfSetData.get(i);
+			i++;
+			channelName = listOfSetData.get(i);
+		}
+
+		requestBody = ReusableFunction.readJsonFile(requestFile);
+		logger.info("Read the json successfully..");
+	}
+
+	@Given("^Lets set the AgeOfInsured is\"([^\"]*)\"$")
+	public void lets_set_the_AgeOfInsured_is(String ageOfInsured) throws Throwable {
+		this.ageOfInsured = ageOfInsured;
+		logger.info("Set the AgeofInsured is successfull..");
+	}
+	
+	@Given("^Lets Set the purchasePrice for SSP Is\"([^\"]*)\"$")
+	public void lest_Set_the_purchasePrice_Is(String purchasePrice) throws Throwable {
+		this.purchasePrice = purchasePrice;
+		logger.info("Set the Purchase price successfully..");
+
+	}
+
+	@Given("^Lets Set the product name is\"([^\"]*)\"$")
+	public void lets_Set_the_product_name_is(String productName) throws Throwable {
+		this.productName = productName;
+		logger.info("Set the prodct name is successfully..");
+
+	}
+
+	@Given("^Lets Set the channel name is\"([^\"]*)\"$")
+	public void lets_Set_the_channel_name_is(String channelName) throws Throwable {
+
+		this.channelName = channelName;
+		logger.info("Set the channel name is successfully..");
+
+	}
+
+	@Given("^Set the endPoint url for Spp illustration Service\"([^\"]*)\"$")
+	public void set_the_endPoint_url_for_Spp_illustration_Service(String endPointUrl) throws Throwable {
+
+		this.endPointUrl = endPointUrl;
+		logger.info("Set the illustration url successfully..");
+	}
+
+	public String getRequestBody() {
+
+		requestBody = requestBody.replaceAll(Pattern.quote("{{" + "ageOfInsured" + "}}"), ageOfInsured);
+		requestBody = requestBody.replaceAll(Pattern.quote("{{" + "purchasePrice" + "}}"), purchasePrice);
+		requestBody = requestBody.replaceAll(Pattern.quote("{{" + "productName" + "}}"), productName);
+		requestBody = requestBody.replaceAll(Pattern.quote("{{" + "channel" + "}}"), channelName);
+		logger.info("Set the data in requestbody successfull.." + requestBody);
+		return requestBody;
+	}
+
+	@When("^Send the valid request for SPP Service$")
+	public void send_the_valid_request_for_SPP_Service() throws Throwable {
+		responseBody = WebservicesMethod.POST_METHOD(endPointUrl, getRequestBody(),
+				ReusableFunction.requestHeaders(header));
+		logger.info("Send the request successfully to server.." + responseBody.prettyPrint());
+	}
+
+	@Then("^Lets Validate the response code for SPP Service\"([^\"]*)\"$")
+	public void lets_Validate_the_response_code_for_SPP_Service(String expResponseMSgCode) throws Throwable {
+		try {
+			responseBody.then().root("msgInfo").body("msgCode", Matchers.equalTo(expResponseMSgCode));
+			logger.info("Validation of response message code is successfull..");
+		} catch (AssertionError e) {
+			responseBody.then().body("statusCode", Matchers.equalTo(expResponseMSgCode));
+			logger.info("Validation of response message is successfull..");
+		}
+	}
+
+	@Then("^Lets Validate the response MSG For SPP Service\"([^\"]*)\"$")
+	public void lets_Validate_the_response_MSG_For_SPP_Service(String expResponseMsg) throws Throwable {
+
+		try {
+			responseBody.then().root("msgInfo").body("msg", Matchers.equalTo(expResponseMsg));
+			logger.info("Validation of response message is successfull..");
+		} catch (AssertionError e) {
+			logger.info("No response message found..");
+		}
+
+	}
+
+	@Then("^Lets Validate the response AFYP of\"([^\"]*)\"$")
+	public void lest_Validate_the_response_AFYP_of(String expAfyp) throws Throwable {
+		responseBody.then().root("payload").body("premiumAmount.AFYP", Matchers.equalTo(expAfyp));
+		logger.info("Validation of AFYP message is successfull..");
+	}
+
+	@Then("^Lets Validate the PremiumWithoutGST of\"([^\"]*)\"$")
+	public void lest_Validate_the_PremiumWithoutGST_of(String expPremiumWithoutGST) throws Throwable {
+		responseBody.then().root("payload").body("premiumAmount.biInstallmentPremiumTotalWithoutGST",
+				Matchers.equalTo(expPremiumWithoutGST));
+		logger.info("Validation of response value of premiumWithoutGST is successfull..");
+
+	}
+
+	@Then("^Lets Validate the PremiumWithGST\"([^\"]*)\"$")
+	public void lest_Validate_the_PremiumWithGST(String expPremiumWithGST) throws Throwable {
+
+		responseBody.then().root("payload").body("premiumAmount.biInstallmentPremiumBasePlanWithGST",
+				Matchers.equalTo(expPremiumWithGST));
+		logger.info("Validation of response value of premiumWithGST is successfull..");
+
+	}
+
+}
---
+++ DolphinApiTest/src/test/java/com/MLI_DOLPHIN/featurefile/LE_SPP_Service.feature
@@ -0,0 +1,80 @@
+@SSPTest
+Feature: Test functionality of LE SPP service
+Background:
+Given Set the prerequiest set of data for LE SPP Service 
+|EndPoint              |Header                                              |RequestFile         |AgeOfInsured|purchasePrice |ProductName                 |Channel|
+|/developer/lepremium  |x-api-key:DTUDHv9UVG8cVT3qmhiSv1UcnvCduzLf1CI6zCVY  |LE_SPP_Service.json | 45         |800000        |Max Life Saral Pension Plan |A      |
+
+@SmokTesting
+Scenario Outline: Test the valid spp premium caculator service with valid set of test data
+When Send the valid request for SPP Service 
+Then Lets Validate the response code for SPP Service"<ResponseCode>" 
+And Lets Validate the response MSG For SPP Service"<ResponseMSG>" 
+And Lets Validate the response AFYP of"<AFYP>"
+And Lets Validate the PremiumWithoutGST of"<PremiumWithoutGST>"
+And Lets Validate the PremiumWithGST"<PremiumWithGST>" 
+
+Examples: 
+|ResponseCode |ResponseMSG |AFYP  |PremiumWithoutGST |PremiumWithGST| 
+|200          |Success     |800000|800000            |814400        |
+
+@SmokTesting
+Scenario Outline: Test the valid spp illustration generator service with valid set of test data
+Given Set the endPoint url for Spp illustration Service"<EndPointUrl>" 
+When Send the valid request for SPP Service 
+Then Lets Validate the response code for SPP Service"<ResponseCode>" 
+And Lets Validate the response MSG For SPP Service"<ResponseMSG>" 
+
+Examples: 
+|EndPointUrl               |ResponseCode |ResponseMSG |
+|/developer/leillustration |200          |Success     |
+
+
+@FunctionalTest
+Scenario Outline: Test the Functionality of SPP Service with valid set of data 
+Given Lets set the AgeOfInsured is"<AgeOfInsured>"
+And Lets Set the purchasePrice for SSP Is"<PurchasePrice>"
+When Send the valid request for SPP Service
+Then Lets Validate the response code for SPP Service"<ResponseCode>" 
+And Lets Validate the response MSG For SPP Service"<ResponseMSG>" 
+And Lets Validate the response AFYP of"<AFYP>"
+And Lets Validate the PremiumWithoutGST of"<PremiumWithoutGST>"
+And Lets Validate the PremiumWithGST"<PremiumWithGST>" 
+
+Examples:
+|AgeOfInsured|PurchasePrice|ResponseCode|ResponseMSG|AFYP   |PremiumWithoutGST|PremiumWithGST|
+|41          |500000       |200         |Success    |500000 |500000           |509000        |
+|50          |1200000      |200         |Success    |1200000|1200000          |1221600       |
+
+@FunctionalTest
+Scenario Outline: Test the Functionality of SPP Service with invalid set of data 
+Given Lets set the AgeOfInsured is"<AgeOfInsured>"
+And Lets Set the purchasePrice for SSP Is"<PurchasePrice>"
+And Lets Set the product name is"<ProductName>"
+And Lets Set the channel name is"<Channel>"
+When Send the valid request for SPP Service
+Then Lets Validate the response code for SPP Service"<ResponseCode>" 
+And Lets Validate the response MSG For SPP Service"<ResponseMSG>" 
+
+Examples:
+|Channel|ProductName                   |AgeOfInsured|PurchasePrice|ResponseCode|ResponseMSG|
+|A      |Max Life Saral Pension Plan   |39          |500000       |500         |Failure    |                 
+|A      |Max Life Saral Pension Plan   |40          |99000        |500         |Failure    |
+|A      |Max Life Saral Pension Plan   |40          |             |500         |Failure    |
+|B      |Max Life Saral Pension Plan   |40          |120000       |500         |Failure    |
+|A      |XPZ                           |40          |120000       |500         |Failure    |
+
+@FunctionalTest
+Scenario Outline: Test the Functionality of SPP Service with valid set of data 
+Given Set the endPoint url for Spp illustration Service"<EndPointUrl>"
+And Lets set the AgeOfInsured is"<AgeOfInsured>"
+And Lets Set the purchasePrice for SSP Is"<PurchasePrice>" 
+When Send the valid request for SPP Service
+Then Lets Validate the response code for SPP Service"<ResponseCode>" 
+And Lets Validate the response MSG For SPP Service"<ResponseMSG>" 
+
+Examples:
+|EndPointUrl               |AgeOfInsured|PurchasePrice|ResponseCode|ResponseMSG|
+|/developer/leillustration |41          |500000       |200         |Success    |
+|/developer/leillustration |50          |1200000      |200         |Success    |
+
