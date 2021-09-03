3e883d1714d4e443755688e9fb1913b32bf52f34
Abhishek singh <abhisheksingh@monocept.com>
2021-09-03 11:59:46 +0530

    Change DRE Servcie

--- DolphinApiTest/src/test/java/com/MLI_DOLPHIN/runnerclass/TestRunnerClass.java
+++ DolphinApiTest/src/test/java/com/MLI_DOLPHIN/runnerclass/TestRunnerClass.java
@@ -12,7 +12,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(features = "src/test/java/com/MLI_DOLPHIN/featurefile",
 
-//tags="@SSPTest",
+//tags="@DiscrepancyRuleEngineTest",
 tags ="@OauthApI,@IFSC_MICR_API,@TPAIntegrationTest,@DiscrepancyRuleEngineTest,@MYMONEY_API,@LE_FTSP_IllustrationGenerator,@UW_MedicalReportService,@LE_AWPService,@EE_ServiceTest,@LE_LPPSService,@LE_PWPService,@FYPP_PremiumTest,@Dolphin_Push_Test,@DedupeAllScenarios,@PolicyadminAllScenarios,@MyAgentAllScenarios,@SPSTest,@AllTestOf_SAP,@AllScenariosOfWLS,@AllScenarios_Test_For_GIP,@AllTestScenarioForFGEP,@AllTest_of_STP,@SWP_ServiceTest,@SJB_ServiceTest,@OTP_AllTest,@PSM_AllTest,@OSP_AllScenarios,@CIPAllTestScenarios,@ClientLevelCheckAllScenarios,@AllScenariosOfGLIP,@SSPTest",
           glue = { "com.MLI_DOLPHIN.stepDefination" }, plugin = { "pretty",
 				"html:target/cucumber-reports", "json:target/cucumber.json" },strict=true, dryRun =false, monochrome = true)
