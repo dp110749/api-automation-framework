f52432756bd2e8b1b0d243a26d1485275d984c06
Abhishek singh <abhisheksingh@monocept.com>
2021-11-17 17:45:06 +0530

    IFSC MICR Service Updated

--- DolphinApiTest/src/main/java/com/MLI_DOLPHIN/stepDefination/Ifsc_Micr.java
+++ DolphinApiTest/src/main/java/com/MLI_DOLPHIN/stepDefination/Ifsc_Micr.java
@@ -13,36 +13,28 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.qameta.allure.Description;
-import io.qameta.allure.Story;
-import io.restassured.response.Response;
 import junit.framework.Assert;
 
-public class Ifsc_Micr {
+public class Ifsc_Micr extends WebservicesMethod{
 
 	private final static Logger logger = Logger.getLogger(Ifsc_Micr.class.getName());
-	public String endPointUri;
-	public String apiHeader;	
 	public String STATUS_CODE;
-	public String FILE_PATH;
-	public String APIREQUEST;
-	public String RESPONSEBODY;
 	public String TESTREQUEST;
 	public String FINALREQUEST;
-	public Response responseOFbody;
-
+	
 	@Description("To test the functionality with valid input parameter ")
 	@Given("^I want to set the initial End Point URL as \"([^\"]*)\" for test case \"([^\"]*)\"$")
 	public void i_want_to_set_the_initial_End_Point_URL_as_for_test_case(String url, String testname) throws Throwable {
 		logger.info(testname + ":Test execution Started");
-		endPointUri = url;
-		logger.info("Passing url to set :" + endPointUri);
+		endPointUrl = url;
+		logger.info("Passing url to set :" + endPointUrl);
 
 	}
 
-	@When("^I set header as \"([^\"]*)\"$")
-	public void i_set_header_as(String header) throws Throwable {
-		apiHeader = header;
-
+	@When("^I set header as \"([^\"]*)\" and \"([^\"]*)\"$")
+	public void i_set_header_as(String header,String methodType) throws Throwable {
+		this.header = header;
+        method_Type=methodType;
 	}
 
 	@When("^I hit the API with requestbody and Post method \"([^\"]*)\"$")
@@ -50,25 +42,25 @@ public class Ifsc_Micr {
 
 		if (requestBodyPath != null && !requestBodyPath.isEmpty()) {
 			JSONParser jsonParser = new JSONParser();
-			FILE_PATH = System.getProperty("user.dir") + "/ApiRequest/" + requestBodyPath;
-			logger.info("Path of requestbody file is :: " + FILE_PATH);
-			try (FileReader reader = new FileReader(FILE_PATH)) {
+			requestFile = System.getProperty("user.dir") + "/ApiRequest/" + requestBodyPath;
+			logger.info("Path of requestbody file is :: " + requestFile);
+			try (FileReader reader = new FileReader(requestFile)) {
 				Object obj = jsonParser.parse(reader);
-				APIREQUEST = obj.toString();
-				logger.info("Request Body is :: " + APIREQUEST);
+				requestBody = obj.toString();
+				logger.info("Request Body is :: " + requestBody);
 			} catch (FileNotFoundException | ParseException exc) {
 				exc.printStackTrace();
 			}
-			if (APIREQUEST.length() > 0) {
+			if (requestBody.length() > 0) {
 
-				responseOFbody = WebservicesMethod.POST_METHOD(endPointUri, APIREQUEST	,
-						ReusableFunction.requestHeaders(apiHeader));
+				responseBody = WebservicesMethod.Select_API_METHOD(method_Type,endPointUrl, requestBody	,
+						ReusableFunction.requestHeaders(header));
 			} else {
 				logger.info(" Request Body cannot be null or empty!");
 			}
 		}
-		STATUS_CODE = String.valueOf(responseOFbody.getStatusCode());
-		logger.info("Response Body is " + responseOFbody.body().prettyPrint());
+		STATUS_CODE = String.valueOf(responseBody.getStatusCode());
+		logger.info("Response Body is " + responseBody.body().prettyPrint());
 
 	}
 
@@ -79,11 +71,11 @@ public class Ifsc_Micr {
 	}
 	@Then("^I try to validate the response time response type and response app id$")
 	public void i_try_to_validate_the_response_time_response_type_and_response_app_id() throws Throwable {
-		responseOFbody.then().spec(SpecificationFactory.getGenericResponseSpec());
+		responseBody.then().spec(SpecificationFactory.getGenericResponseSpec());
 	}
 	@Then("^I want to validate if response body contains specific string \"([^\"]*)\" and \"([^\"]*)\"$")
 	public void i_want_to_validate_if_response_body_contains_specific_string_and(String bankIfscCode, String bankMicrCode) throws Throwable {
-		String bodyStringValue =responseOFbody.asString();
+		String bodyStringValue =responseBody.asString();
 		Assert.assertTrue(bodyStringValue.contains(bankIfscCode));
 		Assert.assertTrue(bodyStringValue.contains(bankMicrCode));
 	}
@@ -93,12 +85,12 @@ public class Ifsc_Micr {
 		
 		if (requestBodyPath != null && !requestBodyPath.isEmpty()) {
 			JSONParser jsonParser = new JSONParser();
-			FILE_PATH = System.getProperty("user.dir") + "/ApiRequest/" + requestBodyPath;
-			logger.info("Path of requestbody file is :: " + FILE_PATH);
-			try (FileReader reader = new FileReader(FILE_PATH)) {
+			requestFile = System.getProperty("user.dir") + "/ApiRequest/" + requestBodyPath;
+			logger.info("Path of requestbody file is :: " + requestFile);
+			try (FileReader reader = new FileReader(requestFile)) {
 				Object obj = jsonParser.parse(reader);
-				APIREQUEST = obj.toString();
-				JSONObject jsonobject = ReusableFunction.createJSONObject(APIREQUEST);
+				requestBody = obj.toString();
+				JSONObject jsonobject = ReusableFunction.createJSONObject(requestBody);
 				JSONObject jsonRequest = ReusableFunction.replacekeyInJSONObject(jsonobject, jsonkey, value);
 				FINALREQUEST = jsonRequest.toString();
 				logger.info("Request Body is :: " + FINALREQUEST);
@@ -108,13 +100,13 @@ public class Ifsc_Micr {
 		}
 		if (FINALREQUEST.length() > 0) {
 
-			responseOFbody = WebservicesMethod.POST_METHOD(endPointUri, FINALREQUEST,
-					ReusableFunction.requestHeaders(apiHeader));
+			responseBody = WebservicesMethod.Select_API_METHOD(method_Type,endPointUrl, FINALREQUEST,
+					ReusableFunction.requestHeaders(header));
 		} else {
 			logger.info(" Request Body cannot be null or empty!");
 		}
-		STATUS_CODE = String.valueOf(responseOFbody.getStatusCode());
-		logger.info("Response body is ::" + responseOFbody.body().prettyPrint());
+		STATUS_CODE = String.valueOf(responseBody.getStatusCode());
+		logger.info("Response body is ::" + responseBody.body().prettyPrint());
 	}
 	
 	@When("^I hit the API with requestbody \"([^\"]*)\" with passing the micrcode \"([^\"]*)\" value \"([^\"]*)\" and replacing the value of ifsc \"([^\"]*)\" and \"([^\"]*)\"$")
@@ -122,12 +114,12 @@ public class Ifsc_Micr {
        		
 		if (requestBodyPath != null && !requestBodyPath.isEmpty()) {
 			JSONParser jsonParser = new JSONParser();
-			FILE_PATH = System.getProperty("user.dir") + "/ApiRequest/" + requestBodyPath;
-			logger.info("Path of requestbody file is :: " + FILE_PATH);
-			try (FileReader reader = new FileReader(FILE_PATH)) {
+			requestFile = System.getProperty("user.dir") + "/ApiRequest/" + requestBodyPath;
+			logger.info("Path of requestbody file is :: " + requestFile);
+			try (FileReader reader = new FileReader(requestFile)) {
 				Object obj = jsonParser.parse(reader);
-				APIREQUEST = obj.toString();
-				JSONObject jsonobject = ReusableFunction.createJSONObject(APIREQUEST);
+				requestBody = obj.toString();
+				JSONObject jsonobject = ReusableFunction.createJSONObject(requestBody);
 				JSONObject jsonRequest = ReusableFunction.replacekeyInJSONObject(jsonobject, ifsckey, ifscvalue);
 				
 				TESTREQUEST = jsonRequest.toString();
@@ -143,18 +135,18 @@ public class Ifsc_Micr {
 		}
 		if (FINALREQUEST.length() > 0) {
 
-			responseOFbody = WebservicesMethod.POST_METHOD(endPointUri, FINALREQUEST,
-					ReusableFunction.requestHeaders(apiHeader));
+			responseBody = WebservicesMethod.Select_API_METHOD(method_Type,endPointUrl, FINALREQUEST,
+					ReusableFunction.requestHeaders(header));
 		} else {
 			logger.info(" Request Body cannot be null or empty!");
 		}
-		STATUS_CODE = String.valueOf(responseOFbody.getStatusCode());
-		logger.info("Response body is ::" + responseOFbody.body().prettyPrint());
+		STATUS_CODE = String.valueOf(responseBody.getStatusCode());
+		logger.info("Response body is ::" + responseBody.body().prettyPrint());
 
 	}
 	@Then("^I want to validate if response body contains specific string \"([^\"]*)\"$")
 	public void i_want_to_validate_if_response_body_contains_specific_string(String errorMesg) throws Throwable {
-		String bodyStringValue =responseOFbody.asString();
+		String bodyStringValue =responseBody.asString();
 		Assert.assertTrue(bodyStringValue.contains(errorMesg));
 
 	}
