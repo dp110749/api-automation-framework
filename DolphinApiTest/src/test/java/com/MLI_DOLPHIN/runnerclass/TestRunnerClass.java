d22117da3ab206bf5ab8c48905cba9c1e9cea3ab
Abhishek singh <abhisheksingh@monocept.com>
2021-11-23 10:55:58 +0530

    runner class

--- DolphinApiTest/src/test/java/com/MLI_DOLPHIN/runnerclass/TestRunnerClass.java
+++ DolphinApiTest/src/test/java/com/MLI_DOLPHIN/runnerclass/TestRunnerClass.java
@@ -4,18 +4,14 @@ import static io.restassured.RestAssured.reset;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.junit.runner.RunWith;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import com.MLI_DOLPHIN.utilities.ReusableFunction;
-import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
-import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(
+//@RunWith(ExtendedCucumber.class)
+/*@ExtendedCucumberOptions(
 		jsonReport ="target/cucumber.json",
 		toPDF=true,
 		
@@ -23,9 +19,8 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 	   
 		detailedAggregatedReport=false
 		)
-
+*/
 @CucumberOptions(features = "./src/test/java/com/MLI_DOLPHIN/featurefile/",
-
 //tags="@LE_FTSP_IllustrationGenerator",
 tags ="@OauthApI,@IFSC_MICR_API,@TPAIntegrationTest,@DiscrepancyRuleEngineTest,@MYMONEY_API,@LE_FTSP_IllustrationGenerator,@UW_MedicalReportService,@LE_AWPService,@EE_ServiceTest,@LE_LPPSService,@LE_PWPService,@FYPP_PremiumTest,@Dolphin_Push_Test,@DedupeAllScenarios,@PolicyadminAllScenarios,@MyAgentAllScenarios,@SPSTest,@AllTestOf_SAP,@AllScenariosOfWLS,@AllScenarios_Test_For_GIP,@AllTestScenarioForFGEP,@AllTest_of_STP,@SWP_ServiceTest,@SJB_ServiceTest,@OTP_AllTest,@PSM_AllTest,@OSP_AllScenarios,@CIPAllTestScenarios,@ClientLevelCheckAllScenarios,@AllScenariosOfGLIP,@SSPTest",
           glue = { "com.MLI_DOLPHIN.stepDefination" }, plugin = { "pretty",
